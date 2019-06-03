3bfb491d3244fafdb19423c620eb5168b190c210
Sierra B <35674159+SierraDev@users.noreply.github.com>
2019-06-02 23:40:27 -0400

    Add files via upload

---
+++ counter/counterexample.js
@@ -0,0 +1,34 @@
+///// ----- COUNTER ----- /////
+// let count = 0;
+
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+
+// const resetNum = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
+
+// const appRoot = document.getElementById("app");
+
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//     <div>
+//     <h1> Count: {count} </h1>
+//     <button onClick = {addOne}>+1</button>
+//     <button onClick = {minusOne}>-1</button>
+//     <button onClick = {resetNum}>Reset</button>
+//     </div>
+//     );
+
+//     ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
